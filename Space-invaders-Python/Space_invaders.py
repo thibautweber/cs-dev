8636b5d2d4bc615be2f5c816db4305089bdbd5c2
Quentin Gagnepain <quentin.gagnepain@cpe.fr>
2022-01-10 17:36:14 +0100

    fin s3

--- Space-invaders-Python/Space_invaders.py
+++ Space-invaders-Python/Space_invaders.py
@@ -16,15 +16,25 @@ import tkinter as tk
 from tkinter import *           
 from random import randint
 from time import time
+#import classes.Alien 
 
 """ A FAIRE : enlever les classes dans ce code et réussir à importer les classes dans ce code"""
+
+
+
+
+
+
+
+
+
 
 ##Initialisation et declaration de variables methode .height et .width de PhotoImage ne fonctionne pas donc oblige de hardcode les dimensions Partie
 Partie_en_cours=False
 Partie_Perdu=True
 ViesInit=300
 
-#Canevas
+#Zone_jeu
 hauteur=480
 largeur=640
 
@@ -39,11 +49,11 @@ largeur_alien=22
 hauteur_alien=16
 ecart_alien=10
 hauteur_alien_ligne1=50
-nbre_alien_par_ligne=15
+nbre_alien_par_ligne=12
 descente_alien=10
 VitesseDeplacement=10
-VitesseAlien=0.5
-AccelerationAlien=0.05
+VitesseAlien=0.5     #Valeur a changer pour changer niveau
+
 
 #Protections
 nbre_protections=4
@@ -53,8 +63,7 @@ hauteur_protections=15
 resistance_protections=5
 
 #tirs allies et ennemies
-VitesseTir= 1
-tps_entre_tir_alien=50  #rester au dessus de 50 pour eviter de planter
+VitesseTir= 1 
 Tirs=[]
 TempsTir=0
 TirsAlien=[]
@@ -67,32 +76,24 @@ PointsAlien=30
 class vaisseau:
     
     def __init__(self):
-        '''self.apparence=canevas.create_oval(posX-largeur_vaisseau/2,\
-        posY-hauteur_vaisseau/2,posX+largeur_vaisseau/2,\
-        posY+hauteur_vaisseau/2,width=2,outline='white')'''
-
+        
         self.x=posX
         self.y=posY
-        self.apparence=canevas.create_image(self.x,self.y,anchor='center',image=ImageVaisseau)
-
+        self.rectangle=Zone_jeu.create_rectangle(posX-largeur_vaisseau/2,\
+        posY-hauteur_vaisseau/2,posX+largeur_vaisseau/2,\
+        posY+hauteur_vaisseau/2,width=2,fill='white')
 
     def deplacement(self,dir):
         if self.x>=largeur_vaisseau and dir==-1:
             self.x+=VitesseDeplacement*dir
         elif self.x<=largeur-largeur_vaisseau and dir==1:
             self.x+=VitesseDeplacement*dir
-        self.Affichage()
-        
-    def Affichage(self):
-        canevas.coords(self.apparence,self.x,self.y)
+        Zone_jeu.coords(self.rectangle,self.x,self.y)       
+        mw.after(5,self.deplacement)
         
         
-    def ViePerdue(self):
-        canevas.itemconfig(self.apparence,image=ImageDestroy)
-        mw.after(500,self.RetourApparenceNormale)
-        
-    def RetourApparenceNormale(self):
-        canevas.itemconfig(self.apparence,image=ImageVaisseau)
+
+
 
 class Alien:
     
@@ -107,12 +108,11 @@ class Alien:
         Alien.vitesse=VitesseAlien
     
     def Creation(self):
-        #self.apparence=canevas.create_rectangle(self.x-largeur_alien/2,Alien.y-hauteur_alien/2,self.x+largeur_alien/2,Alien.y+hauteur_alien/2,width=2,outline='white')
-        self.apparence=canevas.create_image(self.x,self.y,anchor='nw',image=ImageAlien)
+        self.apparence=Zone_jeu.create_rectangle(self.x-largeur_alien/2,Alien.y-hauteur_alien/2,self.x+largeur_alien/2,Alien.y+hauteur_alien/2,width=2,fill='white')
 
     def Affichage(self):
-        #canevas.coords(self.apparence , self.x-largeur_alien/2 ,Alien.y-hauteur_alien/2 , self.x+largeur_alien/2 ,Alien.y+hauteur_alien/2)
-        canevas.coords(self.apparence,self.x,self.y)    
+        Zone_jeu.coords(self.apparence , self.x-largeur_alien/2 ,Alien.y-hauteur_alien/2 , self.x+largeur_alien/2 ,Alien.y+hauteur_alien/2)
+           
 
 class Tir:
     
@@ -120,13 +120,13 @@ class Tir:
     def __init__(self):
         self.x=vaisseau.x
         self.y=vaisseau.y
-        self.apparence=canevas.create_line(self.x , self.y-4 , self.x ,\
+        self.apparence=Zone_jeu.create_line(self.x , self.y-4 , self.x ,\
         self.y , fill='white')
         self.encours=True
         Tir.Compteur+=1
     
     def Affichage(self):
-        canevas.coords(self.apparence , self.x , self.y-4 , self.x , self.y)
+        Zone_jeu.coords(self.apparence , self.x , self.y-4 , self.x , self.y)
     
     def Deplacement(self):
         if self.encours:
@@ -139,23 +139,22 @@ class Tir:
     def FinTir(self):
         if self.y<0:
             self.encours=False
-            canevas.delete(self.apparence)
+            Zone_jeu.delete(self.apparence)
             del Tirs[0]
             Tir.Compteur-=1
         else:
             for i in ennemie:
-                if i.vivant and self.y>=i.y and self.y<=i.y+hauteur_alien and self.x<=i.x+largeur_alien and self.x>=i.x:
+                if i.vivant and self.y>=i.y and self.y<=i.y+hauteur_alien and self.x<=i.x+largeur_alien/2 and self.x>=i.x-largeur_alien/2:
                     self.Destruction()
-                    canevas.delete(i.apparence)
+                    Zone_jeu.delete(i.apparence)
                     i.vivant=False
-                    Alien.vitesse+=AccelerationAlien
                     Points(PointsAlien)
                     PartieGagnee()
 
 
     def Destruction(self):
         self.encours=False
-        canevas.delete(self.apparence)
+        Zone_jeu.delete(self.apparence)
         del Tirs[0]
         Tir.Compteur-=1
                     
@@ -164,17 +163,17 @@ class TirAlien:
     def __init__(self,i):
         self.x=ennemie[i].x
         self.y=ennemie[i].y
-        self.apparence=canevas.create_line(self.x , self.y-4 , self.x ,\
+        self.apparence=Zone_jeu.create_line(self.x , self.y-4 , self.x ,\
         self.y , fill='white')
         self.encours=True
         self.Deplacement()
 
     def affichage(self):
-        canevas.coords(self.apparence , self.x , self.y-4 , self.x , self.y)
+        Zone_jeu.coords(self.apparence , self.x , self.y-4 , self.x , self.y)
         
     def Deplacement(self):
         if self.encours:
-            self.y+=VitesseTir
+            self.y+=1
             self.affichage()
             self.FinTir()
             mw.after(5,self.Deplacement)
@@ -183,16 +182,15 @@ class TirAlien:
         global Vies
         if self.y>hauteur:
             self.encours=False
-            canevas.delete(self.apparence)
+            Zone_jeu.delete(self.apparence)
             del TirsAlien[0]
         elif self.y>=vaisseau.y-5 and self.y<=vaisseau.y+5 and\
             self.x<=vaisseau.x+largeur_vaisseau/2 and\
             self.x>=vaisseau.x-largeur_vaisseau/2 :
                 self.encours=False
-                canevas.delete(self.apparence)
+                Zone_jeu.delete(self.apparence)
                 del TirsAlien[0]
                 Vies-=1
-                vaisseau.ViePerdue()
                 AffichageVies(Vies)
                 if Vies==0:
                     PartiePerdue()
@@ -201,7 +199,7 @@ class TirAlien:
                 if i.Resistance>0 and self.x>=i.x and self.x<=i.x+largeur_protections and self.y>=Protections.y and self.y<=Protections.y+hauteur_protections:
                     i.Update()
                     self.encours=False
-                    canevas.delete(self.apparence)
+                    Zone_jeu.delete(self.apparence)
                     del TirsAlien[0]
         
 
@@ -213,19 +211,19 @@ class Protections:
         self.x=largeur*self.Compteur/(nbre_protections+1)
         Protections.y=posY_protections
         self.Resistance=resistance_protections
-        self.Apparence=canevas.create_rectangle(self.x,self.y,self.x+largeur_protections,self.y+hauteur_protections,width=2,outline='purple',fill='white')
-        self.VieProtection=canevas.create_text(self.x+largeur_protections/2,self.y+hauteur_protections/2,text=str(self.Resistance),fill='red')
+        self.Apparence=Zone_jeu.create_rectangle(self.x,self.y,self.x+largeur_protections,self.y+hauteur_protections,width=2,outline='purple',fill='white')
+        self.VieProtection=Zone_jeu.create_text(self.x+largeur_protections/2,self.y+hauteur_protections/2,text=str(self.Resistance),fill='red')
         
     def Update(self):
         self.Resistance-=1
         if self.Resistance>0:
-            canevas.itemconfig(self.VieProtection,text=(str(self.Resistance)))
+            Zone_jeu.itemconfig(self.VieProtection,text=(str(self.Resistance)))
         else:
             self.Destruction()
     
     def Destruction(self):
-        canevas.delete(self.Apparence)
-        canevas.delete(self.VieProtection)
+        Zone_jeu.delete(self.Apparence)
+        Zone_jeu.delete(self.VieProtection)
 
         
 def MouvementAlien():
@@ -257,10 +255,10 @@ def Tir_Alien():
     global ennemie,TirsAlien 
     if Partie_en_cours:
         L=[i.vivant for i in ennemie]
-        i=randint(0,len(ennemie)-1)
+        i=randint(0,len(ennemie)-1)        
         if L[i]:
             TirsAlien.append(TirAlien(i))
-            mw.after(tps_entre_tir_alien,Tir_Alien)
+            mw.after(250,Tir_Alien)
         else:
             mw.after(1,Tir_Alien)
 
@@ -271,58 +269,15 @@ def Tir_Alien():
 def AffichageVies(Vies):
     NbVies.config(text='Vies: '+str(Vies))
     
-    
-def Clavier(event):
-    global TempsTir
-    touche=event.keysym
-    if touche=='q' or touche=='Left':
-        vaisseau.deplacement(-1)
-    elif touche=='d' or touche=='Right':
-        vaisseau.deplacement(1)
-    elif touche=='space':
-        TempsTir1=time()
-        if TempsTir1-TempsTir>=TempsEntreTirs or Tirs==[]:
-            TempsTir=TempsTir1
-            tir=Tir()
-            Tirs.append(tir)
-            Tirs[Tir.Compteur-1].Deplacement()
 
-def PartieGagnee():
-    global Partie_en_cours, Partie_Perdu
-    gagne=True
-    for i in ennemie:
-        if i.vivant:
-            gagne=False
-    if gagne:
-        canevas.grid_remove()
-        winlose.config(text='Gagne')
-        winlose.grid()
-        canevas.delete("all")
-        BoutonJouer.grid()
-        BoutonJouer.config(text='Continuer')
-        Alien.vitesse=VitesseAlien
-        Partie_en_cours=False
-        DelTirs()
-        Partie_Perdu=False
 
-def PartiePerdue():
-    global Partie_en_cours, Partie_Perdu
-    canevas.grid_remove()
-    winlose.config(text='Perdu')
-    winlose.grid()
-    canevas.delete("all")
-    BoutonJouer.grid()
-    BoutonJouer.config(text='Rejouer')
-    Alien.vitesse=VitesseAlien
-    Partie_en_cours=False
-    DelTirs()
-    Partie_Perdu=True
-    
-    
+
+
+    Protections.Compteur=0
+    Defences=[Protections() for i in range(nbre_protections)]
 def NouvellePartie():
     global ennemie, vaisseau, Partie_en_cours, Score, Vies, Defences
-    canevas.grid()
-    canevas.create_image(0,0,anchor='nw',image=ImageFond)
+    Zone_jeu.grid()
     winlose.grid_remove()
     BoutonJouer.grid_remove()
     vaisseau=vaisseau()
@@ -335,7 +290,9 @@ def NouvellePartie():
         score.config(text='Score: '+str(Score))
     ennemie=[]
     AffichageVies(Vies)
-    Alien.Compteur=0
+    Alien.Compteur=0    
+    Protections.Compteur=0
+    Defences=[Protections() for i in range(nbre_protections)]
     for i in range(nbre_alien_par_ligne):
         ennemie.append(Alien())
     for i in ennemie:
@@ -348,12 +305,28 @@ def Points(pts):
     Score+=pts
     score.config(text='Score: '+str(Score))
 
+
+def Clavier(event):
+    global TempsTir
+    touche=event.keysym
+    if touche=='Left':
+        print("Gauche")
+        dir=-1
+        vaisseau.deplacement(dir)
+    elif touche=='Right':
+        print("Droite")
+        dir=1
+        vaisseau.deplacement(dir)
+    elif touche=='space':
+        TempsTir1=time()
+        if TempsTir1-TempsTir>=TempsEntreTirs or Tirs==[]:
+            TempsTir=TempsTir1
+            tir=Tir()
+            Tirs.append(tir)
+            Tirs[Tir.Compteur-1].Deplacement()
+
 mw=tk.Tk()
 
-ImageVaisseau=tk.PhotoImage(file='images/vaisseau.gif')
-ImageDestroy=tk.PhotoImage(file='images/vaisseaumort.gif')
-ImageAlien=tk.PhotoImage(file='images/invader.gif')
-ImageFond=tk.PhotoImage(file='images/space.gif')
 
 score=tk.Label(mw,text='Score: 0')
 score.grid(row=1,column=1)
@@ -361,11 +334,11 @@ score.grid(row=1,column=1)
 NbVies=tk.Label(mw,text="Vies: 3")
 NbVies.grid(row=1,column=2)
 
-canevas=tk.Canvas(mw,height=hauteur,width=largeur, bg='black')
-canevas.grid(row=2,column=1,columnspan=2)
-canevas.grid_remove()
-canevas.focus_set()
-canevas.bind('<Key>',Clavier)
+Zone_jeu=tk.Canvas(mw,height=hauteur,width=largeur, bg='black')
+Zone_jeu.grid(row=2,column=1,columnspan=2)
+Zone_jeu.grid_remove()
+Zone_jeu.focus_set()
+Zone_jeu.bind('<Key>',Clavier)
 
 BoutonJouer=tk.Button(mw,text='Jouer',command=NouvellePartie)
 BoutonJouer.grid(row=0,column=1)
